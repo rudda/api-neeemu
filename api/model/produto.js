274ea516a033ab838bc8a7aebde0eb0fa1442777
Ruddá Beltrão <beltrao.rudah@gmail.com>
2018-07-17 06:03:53 -0400

    function to add recomendacao to product

--- api/model/produto.js
+++ api/model/produto.js
@@ -13,30 +13,45 @@ connection.connect();
 
 var produto = {
 
-    getProdutos: function(limit){
+    getProdutos: function(limit=25){
        console.log("get-produto");
 
+       var produtos_;
         connection.query('SELECT * from produto limit 0, '+limit, function (error, results, fields) {
             if (error) return false;
-
-            for(var i =0; i<result.legth; i++){
-            
-                connection.query('SELECT * from produto limit 0, '+limit, function (error, results_, fields) {
-
-                    results['recomendacao'] = results_;
-                        
-    
-                });
-                       
-
-            }
             
 
+          }).on('result', function(row){
             
-            console.log("get-produtos", results);
-            return results;
+            console.log("stream", row);
+            var aux = row;
+            var recomendacao;
 
-          });
+            connection.query('SELECT * from produto where produto_id in (select id_produto_recomendacao from recomendacao_produto where recomendacao_produto.id_produto_referencia = produto.produto_id) and produto_id = '+row.produto_id, function (error, results_, fields) {
+
+                if(error) return false;
+                
+                recomendacao = results_;
+
+            }).on('end', function(){
+              
+                aux['recomendacoes'] = recomendacao;
+                produtos_.push(aux);
+
+                console.log("push", produtos_);
+
+            });
+                   
+
+        }).on('end', function(){
+
+            console.log("end", produtos_);
+
+            return produtos_;
+
+        });
+            
+            
 
 
 
@@ -57,10 +72,9 @@ var produto = {
                 return false;
             
             }
-
           
-                console.log("get-produtos", results);
-                produto = results;
+               console.log("get-produtos", results);
+               produto = results;
 
           }).on('end', function(){
 
@@ -92,6 +106,27 @@ var produto = {
 
         console.log("add-recomendacao");
 
+        if(produto_id != produto_recomendacao){
+
+            connection.query('insert into recomendacao_produto set ?',{id_produto_recomendacao : produto_id, id_produto_referencia : produto_recomendacao}, function (error, results, fields) {
+
+               if(error) return false;
+
+            }).on('end', function(){
+                
+              return true;
+
+            });
+
+
+
+        }else{
+
+            return false;
+            console.log("erro", "ids iguais");
+
+        }
+        
 
     }
     
